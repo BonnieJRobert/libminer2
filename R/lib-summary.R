92d4e8e4777293c2cc5b45bc49b7d4f3335b127b
Bonnie Robert <bonnie.robert@gov.bc.ca>
2023-08-03 15:22:08 -0700

    Import purrr::map_dbl

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -15,12 +15,9 @@ lib_summary <- function(sizes = FALSE){
   names(pkg_df) <- c("library", "n_packages")
 
   if (sizes) {
-    pkg_df$lib_size <- vapply(
+    pkg_df$lib_size <- map_dbl(
       pkg_df$library,
-      function(x){
-        sum(fs::file_size(fs::dir_ls(x, recurse =TRUE)))
-      },
-      FUN.VALUE = double(1)
+      ~ sum(fs::file_size(fs::dir_ls(.x, recurse =TRUE)))
     )
   }
 
--- R/libminer2-package.R
+++ R/libminer2-package.R
@@ -2,5 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom purrr map
+#' @importFrom purrr map_dbl
 ## usethis namespace: end
 NULL
